514dce59ec40f22c76016b61c87ff463643d67d5
CamillaEva <caha0007@stud.kea.dk>
2024-11-22 11:43:20 +0100

    cpr-omregner ægte

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Medlem medlem;
 
-        Medlem medlem1 = new Medlem("Camilla", "061026", true, "konkurrence");
+        Medlem medlem1 = new Medlem("Camilla", "221222", true, "konkurrence");
 
         medlem1.cprOmregning();
         System.out.println(medlem1);
