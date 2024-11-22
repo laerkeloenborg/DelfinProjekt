d7924f994b2b698e781c184985ce22f1127283b7
KasperPersson <kape0010@stud.kea.dk>
2024-11-22 10:20:37 +0100

    fileHandler

---
+++ src/main/java/FileHandler.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileHandler {
+
+public ArrayList<Medlem> gemListeAfMedlemmer(ArrayList<Medlem> navnPåListe) {
+    PrintStream output = null;
+    try {
+        output = new PrintStream(new File("medlemsListe"));
+    } catch (FileNotFoundException e) {
+        throw new RuntimeException(e);
+    }
+    for (Medlem medlem : navnPåListe) {
+        output.println(medlem);
+    }
+    return navnPåListe;
+}
+
+public ArrayList<Medlem> hentListeAfMedlemmer(){
+    ArrayList<Medlem> medlemsListe = new ArrayList<>();
+    Scanner scanner = null;
+    File file = new File("medlemsListe");
+    try {
+        scanner = new Scanner(file);
+    } catch (FileNotFoundException e) {
+        throw new RuntimeException(e);
+    }
+    Medlem medlem = null;
+    while (scanner.hasNext()) {
+        String line = scanner.nextLine();
+        String[] attributes = line.split(";");
+        medlem = new Medlem(attributes[0],
+                Integer.parseInt(attributes[1]),
+                Boolean.parseBoolean(attributes[2]),
+                Boolean.parseBoolean(attributes[3]));
+
+        medlemsListe.add(medlem);
+    }
+    scanner.close();
+    return medlemsListe;
+}
+}
