47dac4ab05a353d789e6109f040788508f8f0667
lalo0001@stud.kea.dk <laerkeloenborg@MacBook-Air-tilhrende-Lrke.local>
2024-11-22 11:18:33 +0100

    Konkurrence svømmer klasse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,11 @@ public class Main {
         Medlem medlem;
 
         Medlem medlem1 = new Medlem("Camilla", "061026", true, "konkurrence");
-        Medlem medlem2 = new Medlem("Lærke", "012012", true, "motionist");
+        Medlem medlem2 = new Medlem("Lærke", "231119", true, "motionist");
 
         medlem1.cprOmregning();
         System.out.println(medlem1);
+        medlem2.cprOmregning();
         System.out.println(medlem2);
     }
 }
--- src/main/java/Medlem.java
+++ src/main/java/Medlem.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
-public class Medlem {
+public abstract class Medlem {
     private String navn;
     private String cpr;
     private boolean aktivitetsStatus;
@@ -59,12 +59,11 @@ public class Medlem {
         this.aktivitetsForm = aktivitetsForm;
     }
 
-
     //Metode til omregning af cpr til alder
     public String cprOmregning() {
         LocalDate dato = LocalDate.now();
         DateTimeFormatter formaterDato = DateTimeFormatter.ofPattern("ddMMyy");
-        String formatteretDato = dato.format(formaterDato);
+        String formatteretDato = dato.format(formaterDato); //nuværende dato formatteret
         int resultat = Integer.parseInt(formatteretDato) - Integer.parseInt(this.getCpr());
         String resultat2 = String.valueOf(resultat);
         String deSidsteTo = resultat2.substring(resultat2.length() - 2);
@@ -74,10 +73,10 @@ public class Medlem {
 
     @Override
     public String toString() {
-        return "navn: " + getNavn() +
-                " Alder: " + cprOmregning() +
-                " aktivitets status: " + getAktivitetsStatus() +
-                " aktivitetsform: " + getAktivitetsForm();
+        return "Navn: " + getNavn() +
+                ", Alder: " + cprOmregning() + " år" +
+                ", Aktivitets status: " + (getAktivitetsStatus() ? "Aktiv" : "Passiv") +
+                ", Aktivitetsform: " + getAktivitetsForm();
     }
 }
 
