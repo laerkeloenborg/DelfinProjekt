27ad53aa907d604a43d95facbeb00d26222094b1
christianvinther <christian@vinther.dk>
2024-11-25 09:08:28 +0100

    Formand klasse + metoder til tilføj, slet, print, rediger.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Medlem medlem;
 
+
+
+
         Medlem medlem1 = new Medlem("Camilla", "221222", true, "konkurrence");
 
         medlem1.cprOmregning();
