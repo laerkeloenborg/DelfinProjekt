151c4fb174d3764e77fd122a039914ecf0519a41
CamillaEva <caha0007@stud.kea.dk>
2024-12-02 09:43:41 +0100

    konkurrenceudtagning

--- src/main/java/Træner.java
+++ src/main/java/Træner.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
+
 public class Træner {
+
+    private ArrayList<Medlem> medlemsListe;
 
     public Træner(){
 
+    }
+
+
+
+
+
+    //___________________________metode til at udtage konkurrence svømmerne fra medlemsfilen____________________________
+    public String konkurrenceUdtagning() {
+        String udskriv = " ";
+        int tæller = 1;
+        for (Medlem medlem : medlemsListe) {
+            if (medlem.getAktivitetsForm() == "konkurrence" && medlem.getMedlemsstatus() == MedlemsStatus.AKTIV ) {
+                udskriv += tæller++ + ". " + medlem.getNavn() + ", " + medlem.getCpr() + "(" + medlem.cprOmregningTilAlder() +
+                        " år), " + medlem.getMedlemsstatus() + ", " + medlem.getSvømmedisciplin() + ", " + medlem.getBedsteTid();
+            }
+        }
+        return udskriv.isEmpty() ? "ingen medlemmer i restnace" : udskriv;
     }
 
 }
